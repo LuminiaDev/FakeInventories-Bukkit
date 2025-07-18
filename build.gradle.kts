d8b025999038bebcf4bcb3951666c3eda3cb7b14
Arkadiy <mefrreex@gmail.com>
2025-07-19 03:07:27 +0400

    chore: use gradle 2

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("java")
+}
+
+group = "com.luminiadev.fakeinventories.bukkit"
+version = "1.0.3"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<ProcessResources> {
+    filesMatching("plugin.yml") {
+        expand(project.properties)
+    }
+}
